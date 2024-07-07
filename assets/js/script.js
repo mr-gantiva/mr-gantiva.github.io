5637f1dcf522b299cf6115bbce54a7663707f2c1
mr-gantiva <ivaganva@gmail.com>
2024-07-07 19:56:07 -0400

    Add jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,35 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// $(document).ready(function(){
+//   $("a").on('click', function(event){
+//     if (this.hash !== "") {
+//       event.preventDefault();
+//       var hash = this.hash;
+
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 800, function(){
+//         window.location.hash = hash;
+//       });
+//     }
+// })
+
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
